ac4ae4cb203dbd070596e39653a3f11c7589afd8
shmachid-nssi <59212725+shmachid-nssi@users.noreply.github.com>
2020-03-07 18:17:16 +0900

    set repos

--- scripts/create_pull_request.rb
+++ scripts/create_pull_request.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
 client.create_pull_request(
-  'kmdsbng/zipcode_jp',  # 適当に変える
+  'shmachid-nssi/zipcode_jp',  # 適当に変える
   'master',
   ENV['BRANCH'],
   'Zip data updated',  # Title
